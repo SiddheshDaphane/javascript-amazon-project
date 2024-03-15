8ac7289865239e5c1951b80156202bb76183c37d
SiddheshDaphane <siddhesh.daphane@gmail.com>
2024-03-15 09:11:39 -0500

    Amazon Clone

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -6,7 +6,7 @@ Main idea of JavaScript
 
 */
 
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -49,7 +49,7 @@ cart.forEach((cartItem) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary js-delete-link" data-product-Id="${matchingProduct.id}">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -111,8 +111,9 @@ document.querySelectorAll('.js-delete-link')
   .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
-      console.log(productId);
+      removeFromCart(productId);
+      console.log(cart);
     })
   })
 
-  
+
